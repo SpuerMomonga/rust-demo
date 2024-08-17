b0af2a5cba9ae0dc3062cb28462dc18a9157ba1b
SpuerMomonga <1638829841@qq.com>
2024-08-17 23:51:08 +0800

    feat: 子进程修改

--- crates/child-process/src/main.rs
+++ crates/child-process/src/main.rs
@@ -1,3 +1,11 @@
+use std::thread::sleep;
+use std::time::Duration;
+
+use log::info;
+
 fn main() {
-    println!("子进程启动！");
+    env_logger::init();
+    info!("子进程启动！");
+    sleep(Duration::from_secs(5));
+    info!("子进程结束！");
 }
--- crates/host-process/src/main.rs
+++ crates/host-process/src/main.rs
@@ -1,17 +1,38 @@
 use std::env;
 use std::process::{Command, Stdio};
+use std::thread::sleep;
+use std::time::Duration;
+
+use env_logger::{Builder, Target};
+use log::{info, LevelFilter};
 
 fn main() {
-    print!("主进程启动");
+    // 输出日志
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .target(Target::Stdout)
+        .init();
+
+    info!("主进程启动");
 
     let cwd = env::current_dir().expect("无法获取当前工作目录");
-    println!("当前工作目录: {:?}", cwd);
+    info!("当前工作目录: {:?}", cwd);
+
+    let log_level = "info";
 
     let mut child = Command::new("./target/debug/child-process.exe")
+        .env("RUST_LOG", log_level)
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .expect("进程未找到！");
-    println!("进程ID: {}", child.id());
-    child.wait().unwrap();
+    info!("进程ID: {}", child.id());
+    loop {
+        let status = child.try_wait().unwrap();
+        if let Some(_) = status {
+            break;
+        }
+        sleep(Duration::from_secs(1));
+    }
+    // child.wait().unwrap();
 }
